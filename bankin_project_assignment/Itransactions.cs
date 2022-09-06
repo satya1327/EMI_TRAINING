d35785f8bb8412d2588404daa5fc82665a4807ce
satyajitdas159@gmail.com <satyajitdas159@gmail.com>
2022-09-06 16:10:34 +0530

    solid applied

--- bankin_project_assignment/Itransactions.cs
+++ bankin_project_assignment/Itransactions.cs
@@ -8,7 +8,7 @@ namespace bankin_project_assignment
 {
     public interface Itransactions
     {
-        public  void Deposite(string account_number);
+        public void Deposite(string account_number);
         public void withdraw(string account_number);
 
         
